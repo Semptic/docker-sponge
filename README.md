bdbccb50e5d53cce9fb25f05f628632ed8ae2d4a
Stefan Kuhn <stefan.kuhn@mailbox.org>
2018-01-01 23:10:29 +0100

    Add docker for sponge forge server

---
+++ README.md
@@ -0,0 +1,31 @@
+# Example usage
+
+To start a minecraft container use:
+```bash
+docker run -v /tmp/mc:/minecraft -e MC_VERSION=1.10.2 -e FORGE_VERSION=12.18.3 -e FORGE_BUILD=2555 -e SPONGE_API_VERSION=8.0.0 -e SPONGE_BUILD=2814 -e JAVA_OPTS="-Xms1G -Xmx2G -XX:+UseG1GC -server" semptic/minecraft-sponge
+```
+Create a volume for the mincraft data. Target folder is `/minecraft`. For example to mount a local folder: `-v /host/directory:/minecraft`.
+
+## Configuration
+
+### Environment Variables
+
+#### Setup Variables
+
+Those settings are only used in the first start
+
+ * Minecraft version: MC_VERSION (e.g.: `-e MC_VERSION=1.10.2`)
+ * Forge version: FORGE_VERSION (e.g.: `-e FORGE_VERSION=12.18.3`)
+ * Forge build id: FORGE_BUILD (e.g.: `-e FORGE_BUILD=2254`)
+ * Sponge api Version: SPONGE_API_VERSION (e.g: `-e SPONGE_API_VERSION=5.2.0`)
+ * Sponge build id: SPONGE_BUILD (e.g: `-e SPONGE_BUILD=2234`)
+
+#### Java options
+
+ * JAVA_OPTS (e.g.: `-e JAVA_OPTS="-Xms1G -Xmx2G -XX:+UseG1GC -server"`)
+
+
+
+
+
+
---
+++ launch.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+
+SPONGE_VERSION="${MC_VERSION}-${FORGE_BUILD}-${SPONGE_API_VERSION}-BETA-${SPONGE_BUILD}"
+SPONGE_DOWNLOAD_URL="https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/${SPONGE_VERSION}/spongeforge-${SPONGE_VERSION}.jar"
+
+FORGE_DOWNLOAD_URL="http://files.minecraftforge.net/maven/net/minecraftforge/forge/${MC_VERSION}-${FORGE_VERSION}.${FORGE_BUILD}/forge-${MC_VERSION}-${FORGE_VERSION}.${FORGE_BUILD}-installer.jar"
+
+SERVER="forge-${MC_VERSION}-${FORGE_VERSION}.${FORGE_BUILD}-universal.jar"
+
+if [ ! -f "/minecraft/${SERVER}" ]; then
+    echo "Installing server, take a coffee"
+
+    mkdir -p /tmp/mc
+
+    cd /tmp/mc
+
+    echo "Downloading sponge ${SPONGE_DOWNLOAD_URL}"
+    curl -s "${SPONGE_DOWNLOAD_URL}" -O
+    
+    echo "Downloading forge ${FORGE_DOWNLOAD_URL}"
+    curl -s "${FORGE_DOWNLOAD_URL}" -O
+
+    cd /minecraft
+
+    if [ "$EULA" != "" ]; then
+        echo "# Generated via Docker on $(date)" > eula.txt
+        echo "eula=$EULA" >> eula.txt
+    fi
+
+    java -jar "/tmp/mc/forge-${MC_VERSION}-${FORGE_VERSION}.${FORGE_BUILD}-installer.jar" --installServer
+
+    mkdir -p mods
+
+    mv "/tmp/mc/spongeforge-${SPONGE_VERSION}.jar" mods/
+
+    rm -r /tmp/mc
+fi
+
+cd /minecraft
+
+echo "Starting minecraft"
+java ${JAVA_OPTS} -jar "${SERVER}"
