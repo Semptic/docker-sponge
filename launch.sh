03d23156a938e268ad9a1f841220b6dfeaac30bb
Stefan Kuhn <stefan.kuhn@mailbox.org>
2018-01-02 14:07:50 +0100

    Remove sponge

--- launch.sh
+++ launch.sh
@@ -14,10 +14,6 @@ setServerProp() {
 
 set -e
 
-SPONGE_DOWNLOAD_URL="https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/${SPONGE_VERSION}/spongeforge-${SPONGE_VERSION}.jar"
-SPONGE="spongeforge-${SPONGE_VERSION}.jar"
-
-
 if [ ${FORGE_VERSION} = "latest" ]; then
   curl -fsSL -o /tmp/forge.json http://files.minecraftforge.net/maven/net/minecraftforge/forge/promotions_slim.json
   FORGE_VERSION=$(cat /tmp/forge.json | jq -r ".promos[\"$MC_VERSION-latest\"]")
@@ -50,23 +46,6 @@ if [ ! -f "/minecraft/${SERVER}" ]; then
     echo $OPS > /minecraft/ops.txt
 
     mkdir -p /minecraft/mods
-
-    cd /minecraft
-
-    rm -r /tmp/mc
-fi
-
-if [ ! -f "/minecraft/mods/${SPONGE}" ]; then
-    mkdir -p /tmp/mc
-
-    cd /tmp/mc
-    
-    echo "Downloading sponge ${SPONGE_DOWNLOAD_URL}"
-    curl -s "${SPONGE_DOWNLOAD_URL}" -O
-
-    rm -f "/minecraft/mods/spongeforge-*.jar"
-
-    mv "/tmp/mc/${SPONGE}" /minecraft/mods/
 
     cd /minecraft
 
